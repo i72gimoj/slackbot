bf8d19e2aa8c22d5f7cc24b0f59f21571df16b7e
i72gimoj <43404634+i72gimoj@users.noreply.github.com>
2019-05-17 13:56:20 +0200

    initial commit

---
+++ prestashopAPI.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*
+import requests
+from threading import Thread
+from time import sleep
+import xml.etree.ElementTree as ET
+
+
+class PrestashopAPI(Thread):
+    """
+    Clase que se encarga de comunicarse con la API de Prestashop.
+
+    Mediante esta clase, se puede comunicar fácilmente con la API de Prestashop en Python, facilitando su uso.
+    Por ahora, solo funciona con los productos que sean simples, sin combinaciones.
+    Parametros:
+    url: str -- url de la tienda online de prestashop, debe llevar el 'http://' al principio y el '/api' al final
+    key: str -- clave del WebService de Prestashop
+    func_changes: Function -- función a ejecutar cuando haya algún cambio en la lista de productos
+    func_stock:Function -- función a ejecutar cuando haya algún producto bajo de stock
+    """
+
+    def __init__(self, url, key, func_changes, func_stock):
+        """
+        Constructor de la clase PrestashopAPI, realiza una primera llamada a cargar los productos y clientes en el programa.
+        """
+        super(PrestashopAPI, self).__init__()
+        self.url = url
+        self.key = key
+        self.func_changes = func_changes
+        self.func_stock = func_stock
+        self.cambios = ""
+        self.stock = ""
+        self.productos = self._update_products()
+        self.clientes = self._update_customers()
+
+    def run(self):
+        """
+        Función que se encarga de ejecutar el hilo y comprobar periódicamente cambios.
+        """
+        while True:
+            if self._get_changes():
+                self.func_changes(self.cambios)
+            if self._need_supplies():
+                self.func_stock(self.stock)
+            sleep(30)
+
+    def _get_changes(self):
+        """
+        Función que obtiene cambios en la lista de productos.
+        """
+        self.cambios = "Se han realizado los siguientes cambios:\n"
+        change = False
+        productos = self._update_products()
+        if productos != self.productos:
+            change = True
+            for x in productos:
+                cambia = True
+                for y in self.productos:
+                    if x['nombre'] == y['nombre']:
+                        cambia = False
+                if cambia == True:
+                    self.cambios += "Añadido " + x['nombre'] + "\n"
+            for x in self.productos:
+                cambia = True
+                for y in productos:
+                    if x['nombre'] == y['nombre']:
+                        cambia = False
+                if cambia == True:
+                    self.cambios += "Eiiminado " + x['nombre'] + "\n"
+        self.productos = productos
+        self.clientes = self._update_customers()
+        return change
+
+    def _need_supplies(self):
+        """
+        Función que comprueba si algún artículo está bajo de stock, avisándolo.
+        """
+        self.stock = "Estos artículos necesitan re-stock:\n"
+        needed = False
+        for x in self.productos:
+            if x['necesita_stock?'] == True:
+                needed = True
+                self.stock += x['nombre']
+
+        return needed
+
+    def _update_products(self):
+        """
+        Función que retorna la lista de productos de nuestra tienda online.
+
+        Esta función recopila mediante llamadas a la API de Prestashop la lista de productos que se encuentran en
+        nuestra tienda OnLine
+
+        :return: productos: Dict -- lista de productos en la tienda OnLine
+        """
+        productos = []
+        products = requests.get(self.url + "/products", auth=(self.key, ''))
+        tree = ET.fromstring(products.text)
+        tree.find('products')
+        for x in tree.iter('product'):
+            r = requests.get(x.attrib['{http://www.w3.org/1999/xlink}href'], auth=(self.key, ''))
+            product_id = ET.fromstring(r.text)
+            product_id = product_id.find('product')
+            r = requests.get(product_id.find('associations').find('stock_availables').find('stock_available').attrib['{http://www.w3.org/1999/xlink}href'], auth=(self.key, ''))
+            stock_id = ET.fromstring(r.text)
+            stock_id = stock_id.find('stock_available')
+            if int(stock_id.find('quantity').text) < 50:
+                bajo_stock = True
+            else:
+                bajo_stock = False
+            productos.append({'nombre': product_id.find('name').find('language').text,
+                                  'necesita_stock?': bajo_stock})
+        return productos
+
+    def _update_customers(self):
+        """
+        Función que retorna la lista de clientes de nuestra tienda online.
+
+        Esta función recopila mediante llamadas a la API de Prestashop la lista de clientes que tenemos en nuestra
+        tienda OnLine
+
+        :return: clientes: List -- lista de clientes en nuestra tienda OnLine
+        """
+        clientes = []
+        customers = requests.get(self.url + "/customers", auth=(self.key, ''))
+        tree = ET.fromstring(customers.text)
+        for x in tree.iter('customer'):
+            r = requests.get(self.url + "/customers/" +
+                             x.attrib['id'], auth=(self.key, ''))
+            customer_id = ET.fromstring(r.text)
+            customer_id = customer_id.find('customer')
+            clientes.append({'nombre': customer_id.find('firstname').text,
+                             'apellidos': customer_id.find('lastname').text,
+                             'email': customer_id.find('email').text,
+                             'fecha_nacimiento': customer_id.find('birthday').text})
+        return clientes
+
+    def get_products(self):
+        return self.productos
+
+    def get_customers(self):
+        return self.clientes
